db9fdebdee4705d8007456e9c32477093f0b4b87
juansantiago <js.urquiza@gmail.com>
2022-05-07 18:39:46 -0300

    Primera carga del trabajo realizado en repositorio local hasta ahora

---
+++ app.js
@@ -0,0 +1,16 @@
+const PORT = 3000;
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.listen(PORT, () => {
+    console.log('El servidor está funcionando en el puerto ' + PORT);
+});
+
+app.use(express.static(path.join(__dirname, 'public'))); // Declara la carpeta "public" como "pública", por tanto todo lo que siga de "/" son subcarpetas de "public".
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});

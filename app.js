fb182304ab04dc4a33fdd1d9ce96cf232e7b2a32
juansantiago <js.urquiza@gmail.com>
2022-05-12 21:34:44 -0300

    Agregado de register y login

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.use(express.static(path.join(__dirname, 'public'))); // Declara la carpeta "
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
